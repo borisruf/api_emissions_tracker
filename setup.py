ab63039c518a847e4af97d7b3b5b88b803bdcd00
Boris Ruf <boris.ruf@axa.com>
2024-06-13 20:52:33 +0200

    remove json from setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='emissions_tracker',
     packages=find_packages(),
     package_data={'emissions_tracker': ['emission_factors.json']},
-    install_requires=['json', 'openai', 'mockai@git+https://github.com/borisruf/mockai'],
+    install_requires=['openai', 'mockai@git+https://github.com/borisruf/mockai'],
     version='0.1.0',
     description='Estimates emissions linked to AI cloud services.',
     long_description='A Python library that estimates the carbon emissions linked to cloud-based API services, such as AI cloud services, for demonstration purposes.',
